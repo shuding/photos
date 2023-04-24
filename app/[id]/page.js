7a108838be599be3b07c7041638ea3a514a7dd85
Shu Ding <g@shud.in>
2023-04-24 21:21:45 +0200

    use generateStaticParams

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -3,6 +3,10 @@
 import React from 'react'
 import { SatoriAnimated } from '../satori'
 
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }]
+}
+
 export default function Photo({ params }) {
   const { id } = params
 
