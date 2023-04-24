355e43a2cec3cbb78edd93445118036657a1fe7c
Shu Ding <g@shud.in>
2023-04-24 23:25:28 +0200

    Revert "use tw="
    This reverts commit b9ab4f96f700afafcfcb2956ac36358b4a733200.

--- app/page.js
+++ app/page.js
@@ -9,8 +9,15 @@ export default function Photos() {
     <SatoriAnimated>
       <div
         key='photo-container'
-        tw='flex justify-center items-center w-full h-full'
         style={{
+          display: 'flex',
+          justifyContent: 'center',
+          justifyItems: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          flexDirection: 'row',
+          width: '100%',
+          height: '100%',
           gap: 32,
         }}
       >
@@ -20,14 +27,18 @@ export default function Photos() {
             <img
               key={`photo photo-${i + 1}`}
               src={`/photos/${i + 1}.jpg`}
-              tw='max-w-full max-h-full z-[1]'
               style={{
                 flexBasis: '18%',
+                maxWidth: '100%',
+                maxHeight: '100%',
                 borderRadius: '10px',
                 boxShadow: '0 5px 30px -10px rgba(0, 0, 0, 0.8)',
+                zIndex: 1,
               }}
               className='transition-transform duration-300 transform hover:scale-[1.02] cursor-pointer'
-              onClick={() => Router.push(`/${i + 1}`)}
+              onClick={() => {
+                Router.push(`/${i + 1}`)
+              }}
             />
           ))}
       </div>
