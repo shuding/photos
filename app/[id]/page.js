b9ab4f96f700afafcfcb2956ac36358b4a733200
Shu Ding <g@shud.in>
2023-04-24 23:23:32 +0200

    use tw=

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,51 +1,36 @@
 import React from 'react'
 import { SatoriAnimated } from '../satori'
 
-export const dynamic = 'force-static'
 export const dynamicParams = false
-
 export function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }]
 }
 
-export default function Photo({ params }) {
-  const { id } = params
-
+export default function Photo({ params: { id } }) {
   return (
     <SatoriAnimated>
       <div
         key='photo-container'
+        tw='flex flex-wrap justify-center items-center content-center w-full h-full'
         style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          justifyItems: 'center',
-          alignItems: 'center',
-          alignContent: 'center',
           flexDirection: +id % 2 ? 'row' : 'column',
-          width: '100%',
-          height: '100%',
           gap: 32,
         }}
       >
         <img
           key={`photo photo-${id}`}
           src={`/photos/${id}.jpg`}
+          tw='max-w-full max-h-full z-[2]'
           style={{
             flexBasis: '50%',
-            maxWidth: '100%',
-            maxHeight: '100%',
-            borderRadius: '10px',
+            borderRadius: 10,
             boxShadow: '0 5px 30px -10px rgba(0, 0, 0, 0.8)',
-            zIndex: 2,
           }}
         />
         <p
           key='photo-description'
           style={{
-            margin: 0,
             width: 400,
-            letterSpacing: -0.02,
             zIndex: 1,
           }}
         >{`Photo ${id}: lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`}</p>
